2cd16a1397f36369764162dc35f60811a198680b
master3395 <master3395@users.noreply.github.com>
2026-09-04 17:47:28 +0200

    Remove unused unix CommandExt import for Clippy -D warnings.

--- src/installer.rs
+++ src/installer.rs
@@ -9,8 +9,6 @@ use crate::install_webmail_runtime::webmail_health_url;
 use crate::manifest::{self, ManifestSource};
 use crate::model::{InstallerEvent, InstallerStatus, MailSystem};
 use crate::os_support::require_installable_guest;
-#[cfg(unix)]
-use std::os::unix::process::CommandExt;
 use std::process::Stdio;
 use tokio::{
     io::{AsyncBufReadExt, BufReader},
